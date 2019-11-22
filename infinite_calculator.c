3ed1f1474249dc8eddc6f3d3cecbb802f4b34993
dlalsrn <qazzaq1541@gmail.com>
2019-11-22 22:26:48 +0900

    first commit : 덧셈 뺄셈 구현 완료(아직 버그 존재할 가능성 있음)

--- infinite_calculator.c
+++ infinite_calculator.c
@@ -18,6 +18,50 @@ int Find_Point(char* ary)
 	return i;
 }
 
+char* Change_str(char* a)
+{
+	int cnt = 0;
+	int point = 0;
+	//printf("%s\n", a);
+
+	//printf("%s\n", a);
+	for (int i = 0; i < strlen(a); i++)
+	{
+		if (a[i] == '.')
+			point = 1;
+		if ((a[i] == '+' || a[i] == '-') && i != 0)
+		{
+			if (point == 1)
+				point = 0;
+			else
+				cnt++;
+		}
+	}
+	char* temp = (char*)malloc(strlen(a)+cnt+1);
+	temp[strlen(a)+cnt] = '\0';
+	point = 0;
+	for (int i = 0, s = 0; i <= strlen(a); i++, s++)
+	{
+		if (a[i] == '.')
+			point = 1;
+		if ((a[i] == '+' || a[i] == '-') || a[i] == '\0')
+		{
+			if (point == 0)
+			{
+				if (i != 0)
+				{
+					temp[s] = '.';
+					s++;
+				}
+			}
+			else
+				point = 0;
+		}	
+		temp[s] = a[i];
+	}
+	return temp;
+}
+
 char* Round(char* ary, int max_point)
 {
 	char* temp = (char*)malloc(strlen(ary) + 1);
@@ -83,24 +127,50 @@ char* Round(char* ary, int max_point)
 	}
 	else if (temp[0] <= 48)
 	{
-		char * temp2 = (char*)malloc(strlen(temp));
-		for (int i = 0; i < strlen(temp)-1; i++)
-			temp2[i] = temp[i+1];
-		temp2[strlen(temp)-1] = '\0';
+		int cnt = 0;
+		for (int i = 0; ; i++)
+		{
+			if (temp[i+1] == '.' || temp[i+1] == '\0')
+				break;
+			if (temp[i] != '0')
+				break;
+			cnt++;
+		}
+		char * temp2 = (char*)malloc(strlen(temp)+1-cnt);
+		for (int i = 0; i < strlen(temp)-cnt; i++)
+			temp2[i] = temp[i+cnt];
+		temp2[strlen(temp)-cnt] = '\0';
 		return temp2;
 	}
 	else
 		return temp;
 }
+
+char* Clear(char* a)
+{
+	char* temp = (char*)malloc(strlen(a)+1);
+	strcpy(temp, a);
+	int i;
+	for (i = strlen(temp) - 1; temp[i] == '0'; i--)
+		temp[i] = '\0';
+	if (temp[i] == '.')
+		temp[i] = '\0';
+	//printf("%ld\n", strlen(temp));
+	return temp;
+}
+
 void Array_Sum(char* a)
 {
 	char* temp;
 	char oper; // 연산자 저장 변수
 	char* total;
 	char sign; // 부호 변수
-	int count = strlen(a);
+	char* change = Change_str(a);
+	//printf("%s\n", change);
+	//printf("%s\n", change);
+	int count = strlen(change);
 	char* Num = (char*)malloc(count + 1);
-	strcpy(Num, a);
+	strcpy(Num, change);
 	 
 	if (Num[0] == '-')
 	{
@@ -143,6 +213,7 @@ void Array_Sum(char* a)
 
 		temp[max_point] = '.';
 		//printf("시작\n");
+		//printf("%s\n%s\n%c %c\n", first_Num, second_Num, sign, oper);
 		if (oper == '+')
 		{
 			if (sign == '+')
@@ -307,7 +378,7 @@ void Array_Sum(char* a)
 				}
 			}
 		}
-		else
+		else // ? -
 		{
 			if (sign == '-')
 			{
@@ -476,13 +547,20 @@ void Array_Sum(char* a)
 		free(first_Num);
 		free(second_Num);
 		total = Round(temp, max_point);
+		//printf("%s\n", total);
 		free(temp);
 		//printf("%s\n", total);
 		first_Num = (char*)malloc(strlen(total) + 1);
 		strcpy(first_Num, total);
+		free(total);
 		if (*Num == '\0')
 		{
-			printf("%c%s\n", sign, first_Num);
+			//printf("%s\n", first_Num);
+			total = Clear(first_Num);
+			if (sign == '-')
+				printf("%c%s\n", sign, total);
+			else
+				printf("%s\n", total);
 			break;
 		}
 		else
@@ -501,8 +579,8 @@ void Array_Sum(char* a)
 		//Num += second_count;
 		second_Num = (char*)malloc(second_count + 1);
 		//printf("%d %d\n", first_count, second_count);
-		strcpy(second_Num, Num);
-		//strncpy(second_Num, Num, second_count);
+		//strcpy(second_Num, Num);
+		strncpy(second_Num, Num, second_count);
 
 		Num += second_count;
 		//printf("%s\n%s\n", first_Num, second_Num);
